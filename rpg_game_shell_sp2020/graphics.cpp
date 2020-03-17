d975d910ce1e3b0cb0efce8c1528a8e4b179f3c6
karnwatcharasupat <43736781+karnwatcharasupat@users.noreply.github.com>
2020-03-16 23:21:12 -0400

    Shell code

---
+++ rpg_game_shell_sp2020/graphics.cpp
@@ -0,0 +1,80 @@
+#include "graphics.h"
+
+#include "globals.h"
+
+void draw_nothing(int u, int v)
+{
+    uLCD.filled_rectangle(u, v, u+11, v+11, BLACK);
+}
+
+void draw_img(int u, int v, const char* img)
+{
+    int colors[11*11];
+    for (int i = 0; i < 11*11; i++)
+    {
+        if (img[i] == 'R') colors[i] = RED;
+        else if (img[i] == 'Y') colors[i] = 0xFFFF00; // Yellow
+        else if (img[i] == 'G') colors[i] = GREEN;
+        else if (img[i] == 'D') colors[i] = 0xD2691E; // "Dirt"
+        else if (img[i] == '5') colors[i] = LGREY; // 50% grey
+        else if (img[i] == '3') colors[i] = DGREY;
+        else colors[i] = BLACK;
+    }
+    uLCD.BLIT(u, v, 11, 11, colors);
+    wait_us(250); // Recovery time!
+}
+
+void draw_plant(int u, int v)
+{
+    uLCD.filled_rectangle(u, v, u+10, v+10, GREEN);
+}
+
+void draw_wall(int u, int v)
+{
+    uLCD.filled_rectangle(u, v, u+10, v+10, 0xA52A2A);
+}
+
+
+void draw_player1sprite(int u, int v)
+{
+//    pc.printf("Drawing Player 1 Sprite\r\n");
+    uLCD.filled_rectangle(u, v, u+10, v+10, RED);
+}
+void draw_player2sprite(int u, int v)
+{
+//    pc.printf("Drawing Player 2 Sprite\r\n");
+    uLCD.filled_rectangle(u, v, u+10, v+10, GREEN);
+}
+
+void draw_selection(int u, int v)
+{
+//    pc.printf("Drawing Player 2 Sprite\r\n");
+    uLCD.filled_rectangle(u, v, u+10, v+10, LGREY);
+}
+
+
+void draw_upper_status()
+{
+    // Draw bottom border of status bar
+    uLCD.line(0, 9, 127, 9, GREEN);
+    
+    // Add other status info drawing code here
+}
+ 
+void draw_lower_status()
+{
+    // Draw top border of status bar
+    uLCD.line(0, 118, 127, 118, GREEN);
+    
+    // Add other status info drawing code here
+}
+ 
+void draw_border()
+{
+    uLCD.filled_rectangle(0,     9, 127,  14, WHITE); // Top
+    uLCD.filled_rectangle(0,    13,   2, 114, WHITE); // Left
+    uLCD.filled_rectangle(0,   114, 127, 117, WHITE); // Bottom
+    uLCD.filled_rectangle(124,  14, 127, 117, WHITE); // Right
+}
+ 
+ 
---
+++ rpg_game_shell_sp2020/map.cpp
@@ -0,0 +1,149 @@
+#include "map.h"
+
+#include "globals.h"
+#include "graphics.h"
+
+/**
+ * The Map structure. This holds a HashTable for all the MapItems, along with
+ * values for the width and height of the Map.
+ */
+struct Map {
+    HashTable* items;
+    int w, h;
+};
+
+#define NUM_MAPS 2
+static Map maps[NUM_MAPS];
+static int active_map;
+
+/**
+ * The first step in HashTable access for the map is turning the two-dimensional
+ * key information (x, y) into a one-dimensional unsigned integer.
+ * This function should uniquely map (x,y) onto the space of unsigned integers.
+ */
+static unsigned XY_KEY(int X, int Y) {
+    // TODO: Fix me!
+}
+
+/**
+ * This is the hash function actually passed into createHashTable. It takes an
+ * unsigned key (the output of XY_KEY) and turns it into a hash value (some
+ * small non-negative integer).
+ */
+unsigned map_hash(unsigned key)
+{
+    // TODO: Fix me!
+}
+
+void maps_init()
+{
+    // TODO: Implement!    
+    // Initialize hash table
+    // Set width & height
+}
+
+Map* get_active_map()
+{
+    return &maps[active_map];
+}
+
+Map* set_active_map(int m)
+{
+    active_map = m;
+    return &maps[active_map];
+}
+
+void print_map()
+{
+    char lookup[] = {'W', 'D', 'P', 'A', 'K', 'C', 'N',' ','S'};
+    Map* map = get_active_map();
+    for(int j = 0; j < map->h; j++)
+    {
+        for (int i = 0; i < map->w; i++)
+        {
+            MapItem* item = (MapItem*)getItem(map->items, XY_KEY(i, j));
+            if (item) pc.printf("%c", lookup[item->type]);
+            else pc.printf(" ");
+        }
+        pc.printf("\r\n");
+    }
+}
+
+int map_width()
+{
+    // FIX THIS
+    return 42;
+}
+
+int map_height()
+{
+    // FIX THIS
+    return 42;
+}
+
+int map_area()
+{
+}
+
+MapItem* get_current(int x, int y)
+{
+}
+
+MapItem* get_north(int x, int y)
+{
+}
+
+MapItem* get_south(int x, int y)
+{
+}
+
+MapItem* get_east(int x, int y)
+{
+}
+
+MapItem* get_west(int x, int y)
+{
+}
+
+MapItem* get_here(int x, int y)
+{
+    // FIX THIS
+    return NULL;
+}
+
+
+void map_erase(int x, int y)
+{
+}
+
+void add_wall(int x, int y, int dir, int len)
+{
+    for(int i = 0; i < len; i++)
+    {
+        MapItem* w1 = (MapItem*) malloc(sizeof(MapItem));
+        w1->type = WALL;
+        w1->draw = draw_wall;
+        w1->walkable = false;
+        w1->data = NULL;
+        unsigned key = (dir == HORIZONTAL) ? XY_KEY(x+i, y) : XY_KEY(x, y+i);
+        void* val = insertItem(get_active_map()->items, key, w1);
+        if (val) free(val); // If something is already there, free it
+    }
+}
+
+
+void add_plant(int x, int y)
+{
+    MapItem* w1 = (MapItem*) malloc(sizeof(MapItem));
+    w1->type = PLANT;
+    w1->draw = draw_plant;
+    w1->walkable = true;
+    w1->data = NULL;
+    void* val = insertItem(get_active_map()->items, XY_KEY(x, y), w1);
+    if (val) free(val); // If something is already there, free it
+}
+
+void add_character(int x, int y, void * player_data)
+{
+}
+
